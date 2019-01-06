96619f2a60ab00519dc4aa2a95162bc83f7b7db9
Ben Sumser <31936846+omegacoo@users.noreply.github.com>
2019-01-06 13:54:23 -0600

    Attempt at an OOP rebuild/redesign
    Getting there. It can now create a random board.

---
+++ game OOP+.py
@@ -0,0 +1,61 @@
+import random
+import copy
+import os
+import time
+
+class Game:
+    
+    def __init__(self):
+        self.board = Board()
+    
+    
+class Board:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.state = []
+        
+    def initial_board(self):
+        for x in range(self.width):
+            self.state.append([Cell().value])
+            for y in range(self.height):
+                self.state[x].append(Cell().value)
+        return self.state
+    
+    def render(self):
+        
+        print("__" * self.width + "___")
+        
+        for x in range(self.width):
+            print('| ', end='')
+            for y in range(self.height):
+                if self.state[x][y] == 0:
+                    print('  ', end='')
+                else:
+                    print('# ', end='')
+                
+            print('|')
+            
+        print("--" * self.height + "---")
+    
+class Cell:
+    
+    def __init__(self):
+        self.value = random.randint(0, 1)
+    
+    def is_it_alive(self, value):
+        if self.value == 0:
+            return False
+        else:
+            return True
+    
+    def kill(self):
+        self.value = 0
+    
+    def resurrect(self):
+        self.value = 1
+
+board_one = Board(5,5)
+board_one.initial_board()
+board_one.render()
